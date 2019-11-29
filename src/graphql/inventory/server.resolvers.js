b0e6ad1c73c852f1531f883a96072ad9f0ad96a4
sinsky <sinsky@mz.co.kr>
2019-11-30 06:22:54 +0900

    add query

--- src/graphql/inventory/server.resolvers.js
+++ src/graphql/inventory/server.resolvers.js
@@ -2,15 +2,10 @@ import casual from '../server-model';
 import {arrayOf} from '../casual';
 import {listServers} from '../../controllers/inventory/server';
 import {json} from 'express';
-const domain_id = 'domain-9a817e7aeed7';
 export default {
     Query:{
-        servers: async (a,b,{request},context) => {
-            const parm ={
-                domain_id: domain_id
-            };
-
-            const result = await listServers(parm);
+        servers: async (a,args,{request},context) => {
+            const result = await listServers(args);
             return  result.results;
             // return arrayOf(casual.integer(1, 3), casual._server);
         },
